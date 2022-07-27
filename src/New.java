58b2f04e0c8dbdcfa4bfb6b9c2f0f53411e7e320
Aravindkumarraja <aravindkumarraja10@gmail.com>
2022-07-27 06:42:08 -0400

    code added

---
+++ src/New.java
@@ -0,0 +1,14 @@
+
+public class New {
+
+	public static void main(String[] args) {
+
+		String s = "JAVA";
+		System.out.println(s);
+		String s1 ="SELENIUM";
+	    System.out.println(s1);
+	    
+
+	}
+
+}
